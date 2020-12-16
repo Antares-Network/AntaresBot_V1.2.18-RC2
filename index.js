30b1cb34f03a71528c54533824c8e19c9680dcc4
Nate Goldsborough <business@playantares.com>
2020-12-16 09:06:58 -0600

    oops

--- index.js
+++ index.js
@@ -101,7 +101,7 @@ let args = message.content.substring(PREFIX.length).split(' ');
 					args.shift();
 					args.shift();
 					var msg = args.join(" ");
-					bot.channels.cache.get(args[1]).send(msg);
+					bot.channels.cache.get(chanID).send(msg);
 					onsole.log("The user, " +  message.author.username + " ran &say with the message: " + msg);
 				}
 			}
