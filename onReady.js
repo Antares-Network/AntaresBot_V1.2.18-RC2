151fddbec6e5197cde9d9ef3e18cd6ce4c7a2649
Nate Goldsborough <business@playantares.com>
2020-12-19 14:58:34 -0600

    First commit for onReady

---
+++ onReady.js
@@ -0,0 +1,7 @@
+//Nate Goldsborough
+//Antares Network Discord Bot 
+//This project will morph overtime
+//built for discord.js V.12.5.1
+//
+//onReady.js -- this will handle all the tasks that need to happen on bot startup, like connecting to any API's, servers, checking for updates
+//connecting to databases, etc
