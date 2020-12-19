d0a9798d1d8d68ed0a5c956c62b97abeb55584d6
Nate Goldsborough <business@playantares.com>
2020-12-19 15:49:53 -0600

    rename embed to embedHandler, wrote temp functions

---
+++ embedHandler.js
@@ -0,0 +1,18 @@
+//Nate Goldsborough
+//Antares Network Discord Bot 
+//This project will morph overtime
+//built for discord.js V.12.5.1
+//
+//embed.js - This will handle formatting for all types of embeds sent
+
+module.exports = {
+    catEmbed: function(){
+		console.log("A user tried to use function: catEmbed, but it was not enabled");
+    },
+    dogEmbed: function(){
+		console.log("A user tried to use function: dogEmbed, but it was not enabled");
+    },
+    regularEmbed: function(){
+        console.log("A user tried to use function: regularEmbed, but it was not enabled");
+    }
+}
