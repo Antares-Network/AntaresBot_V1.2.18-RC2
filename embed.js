0c102d4a6ef6be01dc6f79fc71d4e829551a42ef
Nate Goldsborough <business@playantares.com>
2020-12-19 14:58:08 -0600

    First commit for embed handler

---
+++ embed.js
@@ -0,0 +1,6 @@
+//Nate Goldsborough
+//Antares Network Discord Bot 
+//This project will morph overtime
+//built for discord.js V.12.5.1
+//
+//embed.js - This will handle formatting for all types of embeds sent
