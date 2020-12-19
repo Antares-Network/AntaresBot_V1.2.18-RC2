6700bf23eb9bb5b20436db57f23c88fb717442d2
Nate Goldsborough <business@playantares.com>
2020-12-19 14:57:03 -0600

    First commit for roleHandler

---
+++ roleHandler.js
@@ -0,0 +1,6 @@
+//Nate Goldsborough
+//Antares Network Discord Bot 
+//This project will morph overtime
+//built for discord.js V.12.5.1
+//
+//roleHandler.js -- this will handle checking if user has a role, assigning roles, removing roles, updating role permissions etc
