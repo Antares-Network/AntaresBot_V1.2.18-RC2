a185e0ac3c19af531cb5cded9af0b8fdc1fe4f9b
Nate Goldsborough <business@playantares.com>
2020-12-27 21:26:57 -0600

    added guildMSG cmd and updated privacy cmd

--- commands/privacy.js
+++ commands/privacy.js
@@ -13,10 +13,10 @@ module.exports = {
             "All data is stored on secured servers. Maximum efforts are taken to keep collected data protected, but absolute security cannot be guaranteed. We are not liable for any damages or stolen information, in which we collect, from our servers.\n\n" +
 
             "**Server Member Agreement**\n" +
-            "By being a member of a server which uses Santa Bot's services or features, you are consenting to the policies outlined in this agreement. If you, the server member, do not agree with any policies outlined in this agreement, you have the right to leave the server in which this bot is a part of.\n\n" +
+            "By being a member of a server which uses Antares Bot's services or features, you are consenting to the policies outlined in this agreement. If you, the server member, do not agree with any policies outlined in this agreement, you have the right to leave the server in which this bot is a part of.\n\n" +
 
             "**Server Administrator Agreement**\n" +
-            "By adding Santa Bot to your server, you are consenting to the policies outlined in this agreement. If you, the server administrator, do not agree with any of the policies outlined in this agreement, you have the right to remove Antares Bot from your server.\n\n" +
+            "By adding Antares Bot to your server, you are consenting to the policies outlined in this agreement. If you, the server administrator, do not agree with any of the policies outlined in this agreement, you have the right to remove Antares Bot from your server.\n\n" +
 
             "**To request the data we store on you to be deleted from our database, please run the command &remove**\n\n" +
             "However the following data will remain: **Server Name, ID, Owner, and previous bot join date**")
--- handlers/roleHandler.js
+++ handlers/roleHandler.js
@@ -6,18 +6,27 @@
 //roleHandler.js -- this will handle checking if user has a role, assigning roles, removing roles, updating role permissions etc
 
 module.exports = {
-    checkAdmin: function(message) {
+    checkAdmin: function (message) {
         if (message.member.hasPermission('ADMINISTRATOR')) {
             //return boolean if user has the specified role (admin)
-        console.log("Checked if " + message.author.username + " is an admin and they are");
-        return true;
+            console.log("Checked if " + message.author.username + " is an admin and they are");
+            return true;
         } else {
             return false;
         }
     },
-    noPermissionMsg: function(message, command) {
+    noPermissionMsg: function (message, command) {
         //send the following message to the channel the command originated
         message.channel.send("You do not have the required permissions to run this command.");
         console.log("User: " + message.author.username + " tried to use command: " + command + ", but did not have the correct permission");
+    },
+    checkBotOwner: function (message) {
+        if (message.author.id === '603629606154666024') {
+            //return boolean true if the user is @nathen418#0002 with user id '603629606154666024'
+            console.log("Checked if " + message.author.username + " is @nathen418#0002 and they are");
+            return true;
+        } else {
+            return false;
+        }
     }
 };
