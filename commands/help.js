8234fad68b92a3257fe7fe24d00b23468aca86ef
Nate Goldsborough <business@playantares.com>
2021-01-16 16:01:02 -0600

    Updated help command

--- commands/help.js
+++ commands/help.js
@@ -1,26 +1,13 @@
-const prefix = require('../commands/prefix');
-const invite = require('../commands/invite');
-const ip = require('../commands/ip');
-const ping = require('../commands/ping');
-const reddit = require('../commands/reddit');
-const cat = require('../commands/cat');
-const dog = require('../commands/dog');
-const privacy = require("../commands/privacy")
 
-
-
-//send help command
-require('../handlers/exceptionHandler');
 module.exports = {
     helpCMD: function (message) {
-        message.channel.send("This is a list of all the commands:");
-        ip.help(message);
-        cat.help(message);
-        dog.help(message);
-        reddit.help(message);
-        prefix.help(message);
-        invite.help(message);
-        ping.help(message);
-        privacy.help(message);
+        message.channel.send("This is a list of all the commands: \n **ip**: Sends in a dm, the Minecraft server run by the bot Developer." + 
+        "\n **cat**: Sends a random picture of a cat." +
+        "\n **dog**: Sends a random picture of a dog." +
+        "\n **reddit**: Sends a random meme from Reddit." +
+        "\n **prefix**: Shows the Prefix for the bot." +
+        "\n **invite**: Sends an invite for the bot and the support server." +
+        "\n **ping**: Sends the ping time of the bot." +
+        "\n **privacy**: Sends in a dm, the privacy policy for the bot.")
     }
 }
