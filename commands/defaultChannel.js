051b367aa0dce7eccc3610857d461ce47c83912f
Nate Goldsborough <business@playantares.com>
2021-01-16 23:26:33 -0600

    update defaultChannel

--- commands/defaultChannel.js
+++ commands/defaultChannel.js
@@ -10,7 +10,7 @@ module.exports = {
             //check to see if a default channel is set for this server yet
             const req = await guildModel.findOne({ GUILD_ID: message.guild.id });
             //if the server has a default channel, send it here
-            message.channel.send(`This server's default channel is: **${req.GUILD_DEFAULT_CHANNEL}**`);
+            message.channel.send(`This server's default channel is: <#${req.GUILD_DEFAULT_CHANNEL}>`);
 
         } else if (roleHandler.checkAdmin(message)) {
             //if the command was sent with an argument, update the guild's default channel, and let the user know
@@ -18,11 +18,11 @@ module.exports = {
             //check to see if a default channel is set for this server yet
             const req = await guildModel.findOne({ GUILD_ID: message.guild.id });
             //if the server has a default channel, send it here
-            message.channel.send(`This server's default channel is: **${req.GUILD_DEFAULT_CHANNEL}**`);
+            message.channel.send(`This server's default channel is: <#${req.GUILD_DEFAULT_CHANNEL}>`);
             if (args[1]) {
                 console.log(args[1]);
                 const doc = await guildModel.findOneAndUpdate({ GUILD_ID: message.guild.id }, { $set: { GUILD_DEFAULT_CHANNEL: args[1] } }, { new: true });
-                message.channel.send(`Set the prefix to ${doc.GUILD_DEFAULT_CHANNEL}`);
+                message.channel.send(`Set the default channel to <#${doc.GUILD_DEFAULT_CHANNEL}>`);
                 await doc.save();
             }
         }
