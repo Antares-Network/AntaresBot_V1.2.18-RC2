de52fb41ee6b27a1e8ed07aaaa28f49db30b0ab9
Nate Goldsborough <business@playantares.com>
2021-01-17 17:48:40 -0600

    add admin permission check

--- commands/scheduleMessage.js
+++ commands/scheduleMessage.js
@@ -1,27 +1,32 @@
 //send a scheduled message
 const exceptionHandler = require('../handlers/exceptionHandler');
 const logToConsole = require('../events/logToConsole');
+const roleHandler = require('../handlers/roleHandler');
+
 
 module.exports = {
     scheduleCMD: function (message, args) {
-        var d = new Date();
-        var timeToSend;
-        if (!isNaN(args[1])) {
-            timeToSend = args[1];
-        } else {
-            timeToSend = "**N/A**"
+        if (roleHandler.checkAdmin(message)) {
+            var d = new Date();
+            var timeToSend;
+            if (!isNaN(args[1])) {
+                timeToSend = args[1];
+            } else {
+                timeToSend = "**N/A**"
+            }
+            args.shift();
+            var msg = args.join(" ");
+
+
+
+            message.author.send("\n\nThe message you want to send has been recieved" +
+                "\nMessage: **" + msg + "**" +
+                "\nTime scheduled: **" + d.toString() + "**" +
+                "\nTime to send: **" + timeToSend + "**" +
+                "\nServer to be sent in: **" + message.guild.name + "**" +
+                "\nChannel to be sent in: **" + message.channel.name + "**" +
+                "\nSent by: **" + message.author.username + "**");
         }
-        args.shift();
-        var msg = args.join(" ");
-
-
-
-        message.author.send("\n\nThe message you want to send has been recieved" +
-            "\nMessage: **" + msg + "**" +
-            "\nTime scheduled: **" + d.toString() + "**" +
-            "\nTime to send: **" + timeToSend + "**" +
-            "\nServer to be sent in: **" + message.guild.name + "**" +
-            "\nChannel to be sent in: **" + message.channel.name + "**" +
-            "\nSent by: **" + message.author.username + "**");
     }
+
 }
