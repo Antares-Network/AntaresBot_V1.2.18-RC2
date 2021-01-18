3e6d6d4d7e3a4f6127d007442b31f64c82a3687b
Nate Goldsborough <business@playantares.com>
2021-01-17 22:16:32 -0600

    added comments

--- commands/cat.js
+++ commands/cat.js
@@ -2,11 +2,14 @@ const embedHandler = require('../handlers/embedHandler');
 const fetch = require('node-fetch');
 const logToConsole = require('../events/logToConsole');
 
+//get a random cat image from the http://aws.random.cat/meow api
 module.exports = {
     catCMD: function (message) {
+        //request a cat from the api
         fetch('http://aws.random.cat/meow')
             .then(res => res.json())
             .then(json => embedHandler.animalEmbed(message, json, "cat"));
+        //send to the console that this command was run
         logToConsole.command(message.guild, message);
     }
 }
