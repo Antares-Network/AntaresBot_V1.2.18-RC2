7c3918dd27ad8f359bc74275af09f623161bcaca
Nate Goldsborough <business@playantares.com>
2021-01-19 16:23:13 -0600

    update help command

--- commands/help.js
+++ commands/help.js
@@ -5,7 +5,8 @@ module.exports = {
         "\n **ip**: Sends in a dm, the Minecraft server run by the bot Developer." + 
         "\n **cat**: Sends a random picture of a cat." +
         "\n **dog**: Sends a random picture of a dog." +
-        "\n **reddit**: Sends a random meme from Reddit." +
+        "\n **reddit** or **meme**: Sends a random meme from Reddit." +
+        "\n **xkcd** or **comic**: Sends a random XKCD comic" +
         "\n **github**: Sends an embed with a link to the github repo for the bot." +
         "\n **prefix**: Shows the Prefix for the bot." +
         "\n **invite**: Sends an invite for the bot and the support server." +
