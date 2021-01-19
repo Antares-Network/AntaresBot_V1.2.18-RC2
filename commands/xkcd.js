4ab56ce04ca4298bd2c885dea33ce6f7291f51a5
Nate Goldsborough <business@playantares.com>
2021-01-19 15:59:01 -0600

    xkcd update

--- commands/xkcd.js
+++ commands/xkcd.js
@@ -1,19 +1,23 @@
-const xkcd = require('xkcd-api');
 const embedHandler = require('../handlers/embedHandler');
 const logToConsole = require('../events/logToConsole');
+const fetch = require('node-fetch');
+
 
 module.exports = {
-    xkcdCMD: async function (message) {
+    xkcdCMD: async function (message, args) {
+        let comicNum = Math.floor(Math.random() * 2413);
 
-        xkcd.random(function(error, response) {
-            if (error) {
-              console.error(error);
-            } else {
-              console.log(reponse);
-            }
-            embedHandler.regularEmbed(message, response, `Random XKCD comic`)
-          });        
-          logToConsole.command(message.guild, message);
+        var url = `http://xkcd.com/${comicNum}/info.0.json`;
+        
 
+
+                //request a dog from the api
+                fetch(url)
+                .then(res => res.json())
+                .then(json => embedHandler.regularEmbed(message, json.img, `Random Meme from Reddit`));
+
+    
+            //send to the console that this command was run
+            logToConsole.command(message.guild, message);
     }
 }
