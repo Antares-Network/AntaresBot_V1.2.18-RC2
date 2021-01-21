82c43347a9f8e277de061df391e475d000c8a284
Nate Goldsborough <business@playantares.com>
2021-01-21 13:44:12 -0600

    Empty commit

--- commands/dog.js
+++ commands/dog.js
@@ -9,7 +9,6 @@ module.exports = {
         fetch('https://dog.ceo/api/breeds/image/random')
             .then(res => res.json())
             .then(json => embedHandler.regularEmbed(message, json.message, 'Random Dog Picture'));
-
         //send to the console that this command was run
         logToConsole.command(message.guild, message);
     }
--- handlers/embedHandler.js
+++ handlers/embedHandler.js
@@ -1,10 +1,4 @@
-//Nate Goldsborough
-//Antares Network Discord Bot 
-//This project will morph overtime
-//built for discord.js V.12.5.1
-//
 //embed.js - This will handle formatting for all types of embeds sent
-
 const Discord = require('discord.js');
 
 module.exports = {
