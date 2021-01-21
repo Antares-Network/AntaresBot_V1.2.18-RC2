8b287a46a8f85faea40f07cd0e7f4aa2ad297ec5
Nate Goldsborough <business@playantares.com>
2021-01-21 10:14:44 -0600

    Empty commit

--- handlers/embedHandler.js
+++ handlers/embedHandler.js
@@ -11,7 +11,7 @@ module.exports = {
   regularEmbed: function (message, imageUrl, type) {
     const Embed = new Discord.MessageEmbed()
       .setColor('#ff3505')
-      .setURL('https://discord.gg//6pZ2wtGANP')
+      .setURL('https://discord.gg/6pZ2wtGANP')
       .setTitle(type)
       .setImage(imageUrl)
       .setFooter(`Delivered in: ${Date.now() - message.createdTimestamp}ms | Antares Bot | ${botVersion}`, 'https://cdn.discordapp.com/icons/649703068799336454/1a7ef8f706cd60d62547d2c7dc08d6f0.png');
