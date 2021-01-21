82c5d0b0ba5659141d594b998d1a53f6cc7a6e0c
Nate Goldsborough <business@playantares.com>
2021-01-21 10:14:34 -0600

    reduce code in embedHandler

--- handlers/embedHandler.js
+++ handlers/embedHandler.js
@@ -14,7 +14,7 @@ module.exports = {
       .setURL('https://discord.gg//6pZ2wtGANP')
       .setTitle(type)
       .setImage(imageUrl)
-      .setFooter(`Delivered in: ${Date.now() - message.createdTimestamp}ms | Antares Bot`, 'https://cdn.discordapp.com/icons/649703068799336454/1a7ef8f706cd60d62547d2c7dc08d6f0.png');
+      .setFooter(`Delivered in: ${Date.now() - message.createdTimestamp}ms | Antares Bot | ${botVersion}`, 'https://cdn.discordapp.com/icons/649703068799336454/1a7ef8f706cd60d62547d2c7dc08d6f0.png');
     message.channel.send(Embed);
   }
 }
