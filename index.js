beec8ac67b89f1e7422d92b11c7223e5664b00cb
Nate Goldsborough <business@playantares.com>
2021-01-23 10:03:21 -0600

    Update to version 1.2.13 and add counting support

--- index.js
+++ index.js
@@ -11,9 +11,14 @@ const messageHandler = require('./handlers/messageHandler');
 const { connect } = require('mongoose');
 const docCreate = require('./events/docCreate');
 const piiUpdate = require('./events/piiUpdate');
+const piiModel = require('./models/pii');
+
 require('dotenv').config();
 require('colors');
-global.botVersion = "1.2.13";
+global.botVersion = "1.2.14";
+// Stores the timeout used to make the bot count if nobody else counts for a set period of
+// time.
+let timeout
 
 //actions to run at bot startup
 bot.on('ready', async () => {
@@ -38,6 +43,40 @@ bot.on("warn", (e) => console.warn(e));
 
 //actions to run when the bot recieves a message
 bot.on('message', async (message) => {
+	const srv = await piiModel.findOne({ GUILD_ID: message.guild.id }); //find the entry for the guild
+	// Stores the current count.
+	let count = Number(srv.GUILD_COUNTING_NUMBER);
+
+
+	// Only do this for the counting channel of course
+	if (bot.channels.cache.filter(c => c.name === 'counting').keyArray().includes(message.channel.id)) {
+		// You can ignore all bot messages like this
+		if (message.member.user.bot) return
+		// If the message is the current count + 1...
+		console.log(message.content)
+		console.log(count + 1)
+		if (Number(message.content) === count + 1) {
+			// ...increase the count
+			count++
+			await piiModel.findOneAndUpdate({ GUILD_ID: message.guild.id }, { $set: { GUILD_COUNTING_NUMBER: count } }, { new: true });
+			// Remove any existing timeout to count
+			if (timeout) bot.clearTimeout(timeout)
+			// Add a new timeout
+			timeout = bot.setTimeout(
+				// This will make the bot count and log all errors
+				() => message.channel.send(++count).catch(console.error),
+				// after 30 seconds
+				30000
+			)
+			// If the message wasn't sent by the bot...
+		} else if (message.member.id !== bot.user.id) {
+			// ...send a message because the person stuffed up the counting (and log all errors)
+			message.delete()
+			// Reset any existing timeout because the bot has counted so it doesn't need to count again
+			if (timeout) bot.clearTimeout(timeout)
+		}
+	}
+
 	//parse commands
 	messageHandler.messageHANDLE(message, bot);
 });
