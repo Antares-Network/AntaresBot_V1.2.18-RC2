38a5ad511f6be2aed55068cccd9665d70eaeecf8
Nate Goldsborough <business@playantares.com>
2021-01-24 08:46:18 -0600

    HOTFIX - updated restart command

--- commands/restart.js
+++ commands/restart.js
@@ -1,6 +1,7 @@
 const roleHandler = require('../handlers/roleHandler');
 const logToConsole = require('../events/logToConsole');
 const onReady = require('../events/onReady');
+var colors = require('colors');
 
 module.exports = {
     restartCMD: async function (message, bot) {
@@ -9,12 +10,12 @@ module.exports = {
             message.channel.send('Restarting...');
             console.log('\n\n\n\n\n\n\n\n')
             bot.destroy();
-            console.log("Signed out of the Discord API");
+            console.log("Signed out of the Discord API".red.bold);
             bot.login(process.env.BOT_TOKEN).catch(e => console.error(e));
             console.log('Trying to login to the Discord API\nPlease wait for a connection'.yellow);
             onReady.event(bot);
-            console.log("Logged into the Discord API".green);
-            console.log("Startup script has run".red);
+            console.log("Logged into the Discord API".green.bold);
+            console.log("Startup script has run".red.bold);
         } else {
             roleHandler.noPermissionMsg(message, "restart")
         }
